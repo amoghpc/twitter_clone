fab5a0a1d7cd7541316f4b9841ff4001825de604
Amogh P C <amogh.pc@tcs.com>
2014-06-03 11:42:45 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterClone::Application.config.secret_key_base = '52a87b6c579803eab95d8ffb79d238afbca050f784c6120bdca868ea5e2831184f3e9e3aef3b07958d09206094fc77d4616ffe44b256a2004b2c787759286811'
